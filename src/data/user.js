37e35dfab18a149151baf3e7967aa131e30d65c3
DavyHosh <davidwmaina89@gmail.com>
2024-04-15 22:18:11 +0300

    Completed assignment

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,8 @@
-const username = "Liza";
-const city = "New York";
+const username = "David";
+const city = "Nairobi";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+export {username, city, image}; 
